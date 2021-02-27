ba858fc035f898c7a367e63a28a51dc847c86fe3
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-27 13:05:26 -0500

    included queuememes function - needs modificationgit status

--- client/src/utils/apiHelper.js
+++ client/src/utils/apiHelper.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const userController = {
-  getUser: function() {
+  getUser: function () {
     return axios.get(`/api/users/me`);
   },
-   // Gets all memes
-   getMemes: function () {
+  // Gets all memes
+  getMemes: function () {
     return axios.get("/api/users/memes");
   },
   // Gets the meme with the given id
@@ -19,7 +19,15 @@ const userController = {
   // Saves a meme to the database
   saveMeme: function (memeData) {
     return axios.put("/api/users/memes", memeData);
-  }
-};
+  },
+  queueMemes: function () {
+    return axios.get('http://memebuild.com/api/1.0/getRecentMemes?limit=100api-key=', {
+      credentials: "include",
+      headers: {
+        'API-KEY': '1ab967b253352c7063b5669a0d4b1f',
+        'Content-Type': 'application/json'
+      },
+  });
+}
 
 export default userController;
