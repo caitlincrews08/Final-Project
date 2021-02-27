655a10738bff7f30a7b1a9a86f49188cb6be60f1
Alfonso James <ajaeguy@gmail.com>
2021-02-27 03:49:58 -0500

    updating heroku

--- client/src/components/partials/SavedFooter.js
+++ client/src/components/partials/SavedFooter.js
@@ -10,7 +10,9 @@ function SavedFooter() {
             <div className='footborder'></div>
             <Container fluid className='savedfooter'>
                 <Link to='/home' ><h3 className='vertical-center'> ◄ Back </h3></Link>
-                <div className="fb-share-button" data-href="https://memeheim.herokuapp.com/" data-layout="button_count" data-size="small"><Link target="_blank" to="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fmemeheim.herokuapp.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</Link></div>
+                {/* <div className="fb-share-button" data-href="https://memeheim.herokuapp.com/" data-layout="button_count" data-size="small">
+                    <Link target="_blank" to="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fmemeheim.herokuapp.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</Link>
+                </div> */}
             </Container>
         </div>
     );
