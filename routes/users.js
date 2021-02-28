4d68b8472d16a01e588540da63c900c69f527efe
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-28 18:07:01 -0500

    created delete memes functionality and modified helper

--- routes/users.js
+++ routes/users.js
@@ -14,28 +14,41 @@ router.get('/me', (req, res) => {
 // get memes
 router.get("/memes", (req, res) => {
   console.log(req.user)
-  
- Meme.find().where('_id').in(req.user.memes).exec((error, dbMemes) => {
+
+  Meme.find().where('_id').in(req.user.memes).exec((error, dbMemes) => {
     if (error)
       return res.status(400).json(error)
 
     res.json(dbMemes);
   })
 
-  });
+});
 
 // save memes
 router.put("/memes", async (req, res) => {
   const memes = req.body;
-  await memes.forEach (async meme => {
+  await memes.forEach(async meme => {
     await Meme.create(meme)
-      .then(({_id}) => User.findByIdAndUpdate(req.user._id, { $push: { memes: _id } }, { new: true }))
+      .then(({ _id }) => User.findByIdAndUpdate(req.user._id, { $push: { memes: _id } }, { new: true }))
       .catch(err => {
         res.json(err);
       });
   });
-  res.json({success: true});
-  });
+  res.json({ success: true });
+});
+
+// delete memes
+// router.put("/memes", async (req, res) => {
+//   const memes = req.body;
+//   await memes.forEach(async meme => {
+//     await User.findByIdAndUpdate(req.user._id, { $pull: { memes: _id } })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+//   res.json({ success: true })
+// });
+
 
 module.exports = router;
 
