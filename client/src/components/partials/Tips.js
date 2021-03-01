4adef7c6437fda1ea721dd828f4d045f2409f9e0
Alfonso James <ajaeguy@gmail.com>
2021-02-28 23:26:00 -0500

    added jokes to tooltip

--- client/src/components/partials/Tips.js
+++ client/src/components/partials/Tips.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col } from 'react-bootstrap';
-
 import { scroller } from 'react-scroll';
 
+
+
 function Tips() {
+
+  const [jokes, setJokes] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const scrollToSection = () => {
     scroller.scrollTo('scrollAnchor', {
@@ -13,22 +18,42 @@ function Tips() {
     });
   };
 
-  useEffect(() => {
-    scrollToSection();
 
-  });
+  // var request = require('request');
+  // let options = {
+  //   url: 'https://geek-jokes.sameerkumar.website/api?format=json',
+  //   method: 'GET'
+  // }
+
+  // request(options, (err, res, body) => {
+  //   if (!err && res.statusCode == 200){
+  //    var jk = body;
+  //    return jk
+  //   };
+    
+  // });
+
+  useEffect(() => {
+    fetch('https://geek-jokes.sameerkumar.website/api?format=json')
+        .then(res => res.json())
+        .then(
+            (body) => {
+                setIsLoaded(true);
+                setJokes(body.joke);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+}, [])
 
   return (
-      <Col id='autoScroll' className='tips'>
-        <br />
-        <p> Scroll up</p>
-        <br />
-        <br />
-        <p>Hints and descriptions will populate(fade animation/auto-scroll) here onhover of select clickable elements (and possibly an easter egg that displays inspirational and/or funny quotes and sayings) </p>
-        <br />
-        <br />
-        <p className='scrollAnchor'></p>
-      </Col>
+    <Col lg={10} id='autoScroll' className='tips'>
+
+        <p>{jokes}</p>
+    
+    </Col>
   );
 };
 
