7a6898eba4d548d4fbde062572daf8796759ca6d
Caitlin Crews <caitlinrcrews@gmail.com>
2021-03-02 22:09:02 -0500

    modified edit page to get user input

--- client/src/components/pages/Edit.js
+++ client/src/components/pages/Edit.js
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Col, Form, Row } from 'react-bootstrap';
-import { PixoImage } from '../../image';
+import { pixo } from '../../image';
 import { Link } from 'react-router-dom';
 import { Store } from '../../store';
 
@@ -8,19 +8,38 @@ const Edit = () => {
 
   const { state } = useContext(Store)
   const [src, onChange] = useState(state.auth.pre_edit);
+  const { dispatch } = useContext(Store);
+  const fileInput = useRef();
+
+
+  useEffect(() => {
+    pixo.attachToFileInput(fileInput.current);
+  }, [])
+
+
+  function sendToEdit(e, props) {
+    let url = e.currentTarget.files[0];
+    fileInput.current.focus();
+    console.log(url)
+    // dispatch({ type: 'SET_LOAD_FOR_EDIT', payload: url });
+  }
+
+
+
 
   return (
     <div className='main'>
       <Col className='editStart mid-section'>
         <Form className='form'>
-          <Form.File id='upload' label='Choose an Image:' />
+          <Form.File id='upload' label='Choose an Image:' ref={fileInput}/>
         </Form>
         <Row><Col className='form'><p className='form'>-or-</p></Col></Row>
         <Row><Col className='form'>Click image to edit your Meme</Col></Row>
         <Row>
           <Col className='form editPreview'>
             <Link to='/image'>
-              <PixoImage src={src} onChange={onChange} id='selected' />
+              {/* <PixoImage src={src} input={fileInput.current} onChange={onChange} id='selected' /> */}
+              <img src={src} alt="potentially dank meme" onClick={() => pixo.edit(src)} className='editPreview'/>
             </Link>
           </Col>
         </Row>
